df6c096f43854684d769b93cc0b4ceca07feecd5
00Ali00 <103453597+00Ali00@users.noreply.github.com>
2022-04-14 00:21:19 +0300

    s

--- end_level.py
+++ end_level.py
@@ -18,7 +18,7 @@ class End_level:
         self.isRun = True
     
     def create_map(self):
-        for row_index, row in enumerate(END_WORLD_MAP):
+        for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row): 
                 x = col_index*TILESIZE
                 y = row_index*TILESIZE
