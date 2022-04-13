01e1abe7c927e607b8938c0c8dee670334e0b11d
00Ali00 <103453597+00Ali00@users.noreply.github.com>
2022-04-13 18:06:36 +0300

    ррр

--- game.py
+++ game.py
@@ -4,24 +4,24 @@ from level import Level
 from end_level import End_level
 class Game:
     def __init__(self):
-        pygame.init() #инициализируем 
-        pygame.display.set_caption('Tanks') #задаем имя
-        pygame.display.set_icon(pygame.image.load('images/Иконка.bmp'))
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) #задаем высоту и ширину нашего экрана
+        pygame.init() # инициализируем 
+        pygame.display.set_caption('Tanks') # задаем имя
+        pygame.display.set_icon(pygame.image.load('images/Иконка.bmp')) # создаем иконку в углу экрана
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) # задаем высоту и ширину нашего экрана
         self.clock = pygame.time.Clock()
-        self.level = Level()
-        self.end_level = End_level()
+        self.level = Level() # класс 
+        self.end_level = End_level() # класс
         
     def run(self):
         isRun = True
         while isRun:
-            for event in pygame.event.get(): #создаем события 
-                if event.type == pygame.QUIT: #если событие QUIT выходим из цикла
+            for event in pygame.event.get(): # создаем события 
+                if event.type == pygame.QUIT: # если пользователь нажал крестик, цикл останавливается 
                     isRun = False
                     # exit()
                     # pygame.quit()
                     # sys.exit()
-            self.screen.fill('black') #заполняем черным цветом
+            self.screen.fill('black') # заполняем экран черным цветом
             
             if self.level.end == True:
                 pygame.mixer.music.pause()
@@ -30,13 +30,11 @@ class Game:
                 self.level.run()
             if self.end_level.isRun == False:
                 isRun = False
-            pygame.display.update() #обновляем дисплей 
-            self.clock.tick(FPS) #тормозим программу до заданного ФПС
+            pygame.display.update() # обновляем экран 
+            self.clock.tick(FPS) # задаем количество выполнений цикла в секунду (FPS)
             #pygame.time.delay(20)
 
-        
-
-if __name__ == '__main__': #проверяет программа или пакет
+if __name__ == '__main__': # проверяет программа или пакет
     pygame.font.init()
     game = Game()
     game.run() 
